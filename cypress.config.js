62a735d2d5df48b6c4be7cfb79539026018746eb
Gabriel Fernandes <gabrielfernandes@Gabriels-MacBook-Pro.local>
2025-02-04 09:45:10 -0300

    refactor: renaming e2e folder

--- cypress.config.js
+++ cypress.config.js
@@ -2,6 +2,8 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
+    specPattern: "cypress/api/**/*.cy.{js,ts,jsx,tsx}",
+    supportFile: "cypress/support/api.js",
     downloadsFolder: false,
     fixturesFolder: false,
     baseUrl: 'https://pokeapi.co/api/',
