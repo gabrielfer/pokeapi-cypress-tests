3d67f744d5247a09a125c1dc5b641331b4118545
Gabriel Fernandes <gabrielfernandes@Gabriels-MacBook-Pro.local>
2025-02-04 09:45:37 -0300

    fix: removing downloadsFolder false from cypress.config.js

--- cypress.config.js
+++ cypress.config.js
@@ -4,7 +4,6 @@ module.exports = defineConfig({
   e2e: {
     specPattern: "cypress/api/**/*.cy.{js,ts,jsx,tsx}",
     supportFile: "cypress/support/api.js",
-    downloadsFolder: false,
     fixturesFolder: false,
     baseUrl: 'https://pokeapi.co/api/',
     setupNodeEvents(on, config) {
