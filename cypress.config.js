d971f5af21f1f1a0021e59d02709172bc53da1f2
Gabriel Fernandes <gabrielfernandes@Gabriels-MacBook-Pro.local>
2025-02-04 09:27:44 -0300

    fix: fixing baseUrl

--- cypress.config.js
+++ cypress.config.js
@@ -2,7 +2,7 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
-    baseUrl: 'https://pokeapi.com/api/',
+    baseUrl: 'https://pokeapi.co/api/',
     setupNodeEvents(on, config) {
     },
   },
