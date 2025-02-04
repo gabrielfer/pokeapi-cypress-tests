7980edc79bcd0dfe5104a252377678aa4e3196ef
Gabriel Fernandes <gabrielfernandes@Gabriels-MacBook-Pro.local>
2025-02-04 09:39:56 -0300

    refactor: removing downloads folder

--- cypress.config.js
+++ cypress.config.js
@@ -2,6 +2,7 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
+    downloadsFolder: false,
     fixturesFolder: false,
     baseUrl: 'https://pokeapi.co/api/',
     setupNodeEvents(on, config) {
